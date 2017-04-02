3353c2f9af2fd8befcaf5ea61964a510c645fdb9
ProfessorSci <ken_sanchez90@yahoo.com>
2017-04-03 01:51:21 +0800

    initoal commit of repository

---
+++ app/src/main/java/com/ardent/team/recall/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.ardent.team.recall;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
